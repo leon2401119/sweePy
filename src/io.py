a7e195e32f3b1c85a0c2c6edfca861a481057c58
leon2401119 <leon2401119.cs04@nctu.edu.tw>
2022-04-27 19:35:33 +0800

    fix regex bug for scientific notation

--- src/io.py
+++ src/io.py
@@ -1,6 +1,7 @@
 import logging
 import re
 import copy
+import random
 
 logging.getLogger(__name__)
 
@@ -9,10 +10,10 @@ def prefill_cmd(configs,GAname,**kwargs):
     GA_spec = configs['GAspecs'][GAname]
 
     argv = GA_spec["io"]["argv"]
-    tokens = re.findall('\w+|\[\w+\]|.',argv)
+    tokens = re.findall('\w+|\[\w+\]|-+\w+=|.',argv)
 
     for idx, token in enumerate(tokens):
-        if token == 'pop':               # reserved
+        if token in ['pop','seed']:      # reserved
             continue
 
         if re.match('\w+',token):        # required arg
@@ -57,7 +58,11 @@ def prefill_cmd(configs,GAname,**kwargs):
         # 3. prefill basics
         try:
             target = configs['basics'][token]
-            tokens[idx] = str(target)
+            assert type(target) in [int,float,str]
+            if type(target) in [int,float]:  # number
+                tokens[idx] = str(target)
+            else:                             # python statement
+                tokens[idx] = str(eval(target))
             logging.info(f'{GAname} argv substitution : "{token}" -> {target}  (basics)')
             continue
         except Exception as e:
@@ -66,7 +71,11 @@ def prefill_cmd(configs,GAname,**kwargs):
         # 4. prefill fixed fileds
         try:
             target = GA_spec['io']['fixed_fields'][token]
-            tokens[idx] = str(target)
+            assert type(target) in [int,float,str]
+            if type(target) in [int,float]:  # number
+                tokens[idx] = str(target)
+            else:                             # python statement
+                tokens[idx] = str(eval(target))
             logging.info(f'{GAname} argv substitution : "{token}" -> {target}  (fixed fields)')
             continue
         except Exception as e:
@@ -74,8 +83,9 @@ def prefill_cmd(configs,GAname,**kwargs):
 
         
         if required:  # token is a required arg and no match was found
-            logging.error(f'No match for "{token}", which is required in the argv of {GAname}')
-            exit()
+            #logging.error(f'No match for "{token}", which is required in the argv of {GAname}')
+            #exit()
+            continue
 
         else:         # token is an optional keyword and is not specified anywhere
             logging.warning(f'No match for "{token}", since it is optional it will be discarded from argv')
@@ -88,7 +98,10 @@ def prefill_cmd(configs,GAname,**kwargs):
 def prefill_env(configs,GAname,**kwargs):
     GA_spec = configs['GAspecs'][GAname]
 
-    raw_env = copy.deepcopy(GA_spec['io']['envs']) # pitfall: shallow copy modifies the contents of configs
+    try:
+        raw_env = copy.deepcopy(GA_spec['io']['envs']) # pitfall: shallow copy modifies the contents of configs
+    except:
+        raw_env = {}
 
     for key, val in raw_env.items():
         # prefill kwargs
@@ -99,14 +112,26 @@ def prefill_env(configs,GAname,**kwargs):
 
 
 def finalize_cmd(cmd, pop):
-    if 'pop' not in cmd:
+    # finalize reserved tokens
+
+    if 'pop ' in cmd:
+        cmd.replace('pop ',f'{pop} ')
+    elif cmd[-3:] == 'pop':
+        cmd = cmd[:-3] + str(pop)
+    else:
         logging.error(f'Cannot substitute pop for "{cmd}"')
         exit()
 
-    return cmd.replace('pop',str(pop))
+    if 'seed ' in cmd:
+        cmd.replace('seed ', str(random.randint(0,1000)))
+    elif cmd[-4:] == 'seed':
+        cmd = cmd[:-4] + str(random.randint(0,1000))
+
+    return cmd.replace('pop ',f'{pop} ')
 
 
 def parse_output(configs, GAname, out_str):
+    #print(out_str) ## DEBUG
     stdout_regex = configs['GAspecs'][GAname]['io']['stdout']
     tokens = re.findall('\w+|\[\w+\]|.', stdout_regex)
 
@@ -119,14 +144,6 @@ def parse_output(configs, GAname, out_str):
 
     for token in tokens:
         if re.match('\w+',token):        # a name is given, which should correspond to a number
-            r = re.search('(\+|-)?[0-9]+', out_str)                             # interger
-            if r:
-                value = int(out_str[r.start():r.end()])
-                logging.debug(f'{GAname} stdout parsing : {token} = {str(value)}')
-                out_dict[token] = value
-                out_str = out_str[r.end():]
-                continue
-            
             r = re.search('[-+]?([0-9]*[.])?[0-9]+([eE][-+]?\d+)?', out_str)    # floating point / scientific notation
             if r:
                 value = float(out_str[r.start():r.end()])
@@ -135,6 +152,13 @@ def parse_output(configs, GAname, out_str):
                 out_str = out_str[r.end():]
                 continue
 
+            r = re.search('[-+]?[0-9]+', out_str)                               # interger
+            if r:
+                value = int(out_str[r.start():r.end()])
+                logging.debug(f'{GAname} stdout parsing : {token} = {str(value)}')
+                out_dict[token] = value
+                out_str = out_str[r.end():]
+                continue
             else:
                 logging.debug(f'{GAname} stdout parsing : "{token}" not matched with any value')
                 return None
